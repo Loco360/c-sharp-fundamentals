7d58be45dc45a08e6fa1022475a5a86434b7be21
Steve Tipton <stipton@innovasi.com>
2015-12-31 06:49:01 -0500

    A basic switch decision statement

--- UsingSwitch/UsingSwitch/Program.cs
+++ UsingSwitch/UsingSwitch/Program.cs
@@ -10,6 +10,26 @@ namespace UsingSwitch
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Type in a super hero's name to see his nickname:");
+            string userValue = Console.ReadLine();
+
+            switch (userValue.ToUpper())
+            {
+                case "BATMAN":
+                    Console.WriteLine("Caped Crusader");
+                    break;
+                case "SUPERMAN":
+                    Console.WriteLine("Man of Steel");
+                    break;
+                case "GREENLANTERN":
+                    Console.WriteLine("Emerald Knight");
+                    break;
+                default:
+                    Console.WriteLine("Does not compute");
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
