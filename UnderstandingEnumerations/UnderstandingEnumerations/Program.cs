fa7e8154870986720c1829651d59aef54b46d6bb
Steve Tipton <stipton@innovasi.com>
2015-12-31 06:56:30 -0500

    Enumerations and the switch Decision Statement

--- UnderstandingEnumerations/UnderstandingEnumerations/Program.cs
+++ UnderstandingEnumerations/UnderstandingEnumerations/Program.cs
@@ -11,9 +11,43 @@ namespace UnderstandingEnumerations
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
+            //Console.WriteLine("Hello world!");
 
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Type in a superhero's name to see his nickname: ");
+            string userValue = Console.ReadLine();
+
+            SuperHero myValue;
+
+            if (Enum.TryParse<SuperHero>(userValue, true, out myValue)) // Enum.TryParse<Class>(inputVar, (bool)ignoreCase, out outputVar)
+            {
+                switch (myValue)
+                {
+                    case SuperHero.Batman:
+                        Console.WriteLine("Caped crusader");
+                        break;
+                    case SuperHero.Superman:
+                        Console.WriteLine("Man of Steel");
+                        break;
+                    case SuperHero.GreenLantern:
+                        Console.WriteLine("Emerald Knight");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Does not compute");
+            }
+
             Console.ReadLine();
         }
+    }
+
+    enum SuperHero
+    {
+        Batman,
+        Superman,
+        GreenLantern
     }
 }
